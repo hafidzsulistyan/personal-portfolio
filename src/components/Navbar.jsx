f4c076241fcf4d8faa99acfad7a3e2ddfbb34122
Muhammad Hafidz S <muhammad.hafidz05@ui.ac.id>
2022-03-24 19:11:35 +0700

    Fix social icons position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className='hidden lg:flex fixed flex-col top-[360%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
